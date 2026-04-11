2f40d07025fa41f309d30e5356b5b99957e0d03c
CARTEL-L <sharonprincewill5@gmail.com>
2026-04-11 19:10:08 +0100

    Add files via upload

---
+++ PythonTask/new assignment/assignment1.py
@@ -0,0 +1,13 @@
+How to make a Cup of Tea
+
+Pour water in a kettle
+put the kettle on the stove
+switch on the stove
+have a sit for a short wile and appreciate nature
+check the water every minute
+water enters 100 degrees celcius take it off the stove
+switch off the stove
+poor the hot water into a cup of your choice
+add in chocolate and milk
+stir it gently
+you have a choice to add sugar  depending the way you want it
---
+++ PythonTask/new assignment/assignment3.py
@@ -0,0 +1,5 @@
+('if it is rainig')
+if raining == true
+    (take an umbrella)
+else raining != true
+    (put on sunscreen)
