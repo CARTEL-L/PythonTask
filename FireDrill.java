1d0267f0a0701afee1e2a4432a02aeff2c52afa0
CARTEL-L <sharonprincewill5@gmail.com>
2026-05-04 18:46:25 +0100

    Add files via upload

---
+++ FireDrill.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class FireDrill{
+ public static void main(String[] args){
+Scanner inputCollector = new Scanner(System.in);
+ System.out.print("Enter Scores: ");
+int sum = 0;
+int []array = new int [10];
+
+    int score = inputCollector.nextInt();
+for (int count = 1; count <= 10; count++){
+
+
+
+
+System.out.print("Enter number: ");
+int number = inputCollector.nextInt();
+for(int counter = number; counter <= 1; counter++){
+
+
+counter = new int [10];
+System.out.println(counter);
+}
+
+}
+
+}
+}
+
+
+
